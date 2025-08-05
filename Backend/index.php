a85a2824bf28a0e421f201fca1919d261b86d79e
jaakka <jaska.talvitie@gmail.com>
2025-08-05 16:06:01 +0300

    test

--- Backend/index.php
+++ Backend/index.php
@@ -1,10 +1,10 @@
 <?php
     header('Content-Type: application/json');
 
-$data = [
-    'message' => 'this is backend',
-    'status' => 'ok'
-];
+    $data = [
+        'message' => 'this is backend',
+        'status' => 'ok'
+    ];
 
-echo json_encode($data);
+    echo json_encode($data);
 ?>
